a8b56b20029951d9b223a053d4d250b287e4b298
Igor Anić <igor.anic@gmail.com>
2025-01-08 20:58:49 +0100

    fix `zig build test`

--- build.zig
+++ build.zig
@@ -30,6 +30,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe_unit_tests.root_module.addImport("iox", iox_mod);
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
